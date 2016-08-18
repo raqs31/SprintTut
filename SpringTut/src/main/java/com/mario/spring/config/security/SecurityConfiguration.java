703eaee6e117a4adeb0e55aeb67c6fab9e86329e
raqs <raqs31@gmail.com>
2016-08-18 22:54:33 +0200

    Add custom login page

--- SpringTut/src/main/java/com/mario/spring/config/security/SecurityConfiguration.java
+++ SpringTut/src/main/java/com/mario/spring/config/security/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.inject.Inject;
 import javax.sql.DataSource;
@@ -31,6 +32,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
+		http.authorizeRequests().antMatchers("/css/**").permitAll().anyRequest()
+				.fullyAuthenticated().and().formLogin().loginPage("/login")
+				.failureUrl("/login?error").permitAll().and().logout().permitAll()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).and()
+				.exceptionHandling().accessDeniedPage("/access?error");
 	}
+
+
 }
--- SpringTut/src/main/java/com/mario/spring/web/GreetingController.java
+++ SpringTut/src/main/java/com/mario/spring/web/GreetingController.java
@@ -1,19 +1,23 @@
 package com.mario.spring.web;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by raqsW on 2016-08-16.
  */
 @Controller
-public class GreetingController {
-	@RequestMapping("/greeting")
-	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
-		model.addAttribute("name", name);
+public class GreetingController extends WebMvcConfigurerAdapter {
+	@GetMapping("/")
+	public String home(Map<String, Object> model) {
+		model.put("message", "Hello World");
+		model.put("title", "Hello Home");
+		model.put("date", new Date());
 		return "greeting";
-
 	}
 }
