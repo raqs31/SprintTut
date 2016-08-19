587a868164525ab9009f8bc93d4d2776cc6e34c0
raqs <raqs31@gmail.com>
2016-08-19 14:48:30 +0200

    ThymeLeaf simple menu

---
+++ SpringTut/src/main/java/com/mario/spring/web/auth/UserProfilleController.java
@@ -0,0 +1,21 @@
+package com.mario.spring.web.auth;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * Created by raqsW on 2016-08-19.
+ */
+@Controller
+@RequestMapping("/auth")
+public class UserProfilleController {
+
+	@GetMapping("/profile")
+	public String userProfile(Map<String, Object> model) {
+
+		return "/auth/profile";
+	}
+}
