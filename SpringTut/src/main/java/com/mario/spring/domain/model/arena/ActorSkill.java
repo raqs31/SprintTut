c59044da571e489751f4ccdb6ef92d9628c3a7fd
raqs <raqs31@gmail.com>
2016-08-22 16:34:59 +0200

    Id for not completed ActorSkill

--- SpringTut/src/main/java/com/mario/spring/domain/model/arena/ActorSkill.java
+++ SpringTut/src/main/java/com/mario/spring/domain/model/arena/ActorSkill.java
@@ -1,13 +1,30 @@
 package com.mario.spring.domain.model.arena;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.mario.spring.domain.model.AuditableEntity;
+import lombok.Data;
+
+import javax.persistence.*;
 
 /**
  * Created by raqsW on 2016-08-22.
  */
+@Data
 @Entity
 @Table(name = "ACTOR_SKILLS")
-public class ActorSkill {
+public class ActorSkill extends AuditableEntity<Integer>{
+	@Id
+	@Column(name = "ACTOR_SKILL_ID")
+	@SequenceGenerator(name = "actorSkillsSeq", sequenceName = "ACTOR_SKILLS_IDX_SEQ")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "actorSkillsSeq")
+	private Integer skillId;
 
+	@Override
+	public Integer getId() {
+		return skillId;
+	}
+
+	@Override
+	public void setId(Integer newId) {
+		this.skillId = newId;
+	}
 }
